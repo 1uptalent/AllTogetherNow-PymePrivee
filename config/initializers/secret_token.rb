99cc06d047256df8b51505a93de593aea429cd96
amuino <amuino@gmail.com>
2011-04-01 18:56:44 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pymeprivee::Application.config.secret_token = '19cca806e60324a282f6731084ab7db48eea2fa8ede4498f316f078f4cf0cf96296ffcbde673de338e4b26769d280c16e621ba102d56c491989af67f187c6003'
