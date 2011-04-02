1ec2914f686ae7c6b76f882a19f8875ac50f1a59
GermanDZ <germ@ndz.com.ar>
2011-04-02 14:34:37 +0200

    Added shops and products to the home. Product is not the correct item to show in the home, must be SaleItem

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
+    @shops = Shop.order("created_at desc").limit(10)
+    @products = Product.order("created_at desc").limit(10)
   end
 
 end
