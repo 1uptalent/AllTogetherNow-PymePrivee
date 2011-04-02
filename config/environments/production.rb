5b64b6708bc6129a00c87c11bbaa12480f3d4426
amuino <amuino@gmail.com>
2011-04-03 01:51:57 +0200

    Use the PayPal sandbox

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,4 +47,8 @@ Pymeprivee::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  config.after_initialize do
+    ActiveMerchant::Billing::Base.mode = :test
+  end
 end
