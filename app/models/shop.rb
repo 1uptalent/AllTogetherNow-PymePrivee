d51b6a96960799cf8414da2aef5dd8a8ec9c3d69
GermanDZ <germ@ndz.com.ar>
2011-04-03 13:39:31 +0200

    Added Login Bar for owners of Virtual Shops

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -7,4 +7,8 @@ class Shop < ActiveRecord::Base
   validates :user, :name, :presence => true
   
   has_attached_file :logo, :styles => { :large => "700x400>", :medium => "250x250>", :thumb => "100x100>" }
+  
+  def virtual_site?
+    !hostname.blank?
+  end
 end
