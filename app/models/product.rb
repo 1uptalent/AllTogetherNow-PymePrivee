f366e48385a673df3e74790667cd2fdf24de3cd8
amuino <amuino@gmail.com>
2011-04-03 11:32:53 +0200

    Add picture to Products

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ActiveRecord::Base
   belongs_to :shop
   
   validates :shop, :presence => true
+  
+  has_attached_file :picture, :styles => { :full => "1024x768>", :large => "700x400>", :medium => "250x250>", :thumb => "100x100>" }
 end
