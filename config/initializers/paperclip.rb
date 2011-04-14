31dd67a1bc2c3b27f07207796cc28fde9a17e2db
amuino <amuino@gmail.com>
2011-04-14 12:14:17 +0200

    FIX additional slash on generated url

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -2,7 +2,7 @@ if Rails.env.production?
   PAPERCLIP_STORAGE_OPTIONS = {:storage => :s3, 
                                :s3_credentials => "#{Rails.root}/config/s3.yml",
                                :bucket => "xgn",
-                               :path => "/:style/:filename",
+                               :path => ":style/:filename",
                                :url  => ":s3_eu_url"}
 else
   PAPERCLIP_STORAGE_OPTIONS = {}
