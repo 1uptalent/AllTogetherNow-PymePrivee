c9eb87563c90dbaf1cfa61c7de14a76c25c6b3b1
amuino <amuino@gmail.com>
2011-04-14 12:07:47 +0200

    Apply monkey patching 'solution' for european buckets

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -2,7 +2,21 @@ if Rails.env.production?
   PAPERCLIP_STORAGE_OPTIONS = {:storage => :s3, 
                                :s3_credentials => "#{Rails.root}/config/s3.yml",
                                :bucket => "xgn",
-                               :path => "/:style/:filename"}
+                               :path => "/:style/:filename",
+                               :url  => ":s3_eu_url"}
 else
   PAPERCLIP_STORAGE_OPTIONS = {}
+end
+
+# FIX for european buckets
+# http://techspry.com/ruby_and_rails/amazons-s3-european-buckets-and-paperclip-in-rails-3/
+
+Paperclip.interpolates(:s3_eu_url) do |att, style|
+    "#{att.s3_protocol}://s3-eu-west-1.amazonaws.com/#{att.bucket_name}/#{att.path(style)}"
+end
+
+module AWS
+    module S3
+        DEFAULT_HOST = "s3-eu-west-1.amazonaws.com"
+    end
 end
